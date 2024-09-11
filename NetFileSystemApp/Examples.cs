3bd1d9fa177b49125d9144ae095034e813465c53
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-09-11 20:51:30 +0300

    11-09-24

--- NetFileSystemApp/Examples.cs
+++ NetFileSystemApp/Examples.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace NetFileSystemApp
 {
@@ -112,6 +113,135 @@ namespace NetFileSystemApp
 
 
 
+
+        }
+
+        public static async Task FileStreamExampleAsync()
+        {
+            string filePath = @"D:\RPO\file01.dat";
+
+            string text = "В следующем примере показано, как выполнять асинхронную запись в файл. Этот код выполняется в приложении WPF с именем TextBlock с именем UserInput и кнопкой, подключенной к обработчику событий Click, который называется Button_Click. Путь к файлу необходимо изменить на файл, который существует на компьютере.";
+            int number = 123;
+            double x = 345.3423;
+
+            //using (FileStream stream = File.Open(filePath, FileMode.Create))
+            //{
+            //    byte[] buffer = Encoding.UTF8.GetBytes(text);
+            //    //byte[] buffer = BitConverter.GetBytes(number);
+            //    //await stream.WriteAsync(buffer, 0, buffer.Length);
+
+            //    //buffer = BitConverter.GetBytes(x);
+            //    await stream.WriteAsync(buffer, 0, buffer.Length);
+
+            //}
+
+            using (FileStream stream = File.Open(filePath, FileMode.Open))
+            {
+
+
+                //await stream.ReadAsync(buffer, 0, 4);
+                //int num = BitConverter.ToInt32(buffer);
+                //Console.WriteLine(num);
+
+                //await stream.ReadAsync(buffer);
+                //double xx = BitConverter.ToDouble(buffer);
+                //Console.WriteLine(xx);
+
+                stream.Seek(21, SeekOrigin.Begin);
+                byte[] buffer = new byte[stream.Length - 21];
+
+                await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                string textRead = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine(textRead);
+            }
+
+        }
+
+        public static async Task TextStreamExampleAsync()
+        {
+            string pathFile = @"D:\RPO\file02.txt";
+
+            string text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+            string text2 = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+
+            //using (StreamWriter writer = new(pathFile))
+            //{
+            //    await writer.WriteLineAsync(text);
+            //}
+
+            //using (StreamWriter writer = new(pathFile, true))
+            //{
+            //    await writer.WriteLineAsync(text2);
+            //    await writer.FlushAsync();
+            //}
+
+            using (StreamReader reader = new(pathFile))
+            {
+                //for(int i = 0; i < 10; i++)
+                //    Console.WriteLine((char)reader.Read());
+
+                //int c;
+                //while((c = reader.Read()) != -1)
+                //    Console.Write((char)c);
+
+                //string? textLine;
+                //while((textLine = await reader.ReadLineAsync()) is not null)
+                //    Console.WriteLine(textLine);
+
+                text = reader.ReadToEnd();
+                Console.WriteLine(text);
+            }
+        }
+
+        public static void BinaryStreamExample()
+        {
+
+
+            //int number = 123;
+            //double x = 345.897;
+            //string str = "Hello world";
+            //bool flag = false;
+
+            //string filePath = @"D:\RPO\file03.bin";
+
+            //Employee employee = new() { Name = "Tommy", Birthday = DateTime.Now, Salary = 150500m };
+
+            //#pragma warning disable SYSLIB0011 // Тип или член устарел
+            //BinaryFormatter formatter = new();
+            //using (FileStream writer = File.OpenWrite(filePath))
+            //{
+            //    formatter.Serialize(writer, employee);
+            //}
+
+#pragma warning restore SYSLIB0011 // Тип или член устарел
+
+            //using(BinaryWriter writer = new(File.OpenWrite(filePath)))
+            //{
+            //    writer.Write(number);
+            //    writer.Write(x);
+            //    writer.Write(str);
+            //    writer.Write(flag);
+            //}
+
+            //using (BinaryReader reader = new(File.OpenRead(filePath)))
+            //{
+            //    int n = reader.ReadInt32();
+            //    double y = reader.ReadDouble();
+            //    string s = reader.ReadString();
+            //    bool f = reader.ReadBoolean();
+
+            //    Console.WriteLine($"n = {n}\ny = {y}\ns = {s}\nf = {f}");
+            //}
+
+
+            //[Serializable]
+            //class Employee
+            //{
+            //    public string? Name { get; set; }
+            //    public DateTime Birthday { get; set; }
+            //    public decimal Salary { get; set; }
+            //}
 
         }
     }
--- NetFileSystemApp/Program.cs
+++ NetFileSystemApp/Program.cs
@@ -1,31 +1,82 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
-string filePath = @"D:\RPO\file01.dat";
+string fileJson = @"D:\RPO\object.json";
 
-string text = "В следующем примере показано, как выполнять асинхронную запись в файл. Этот код выполняется в приложении WPF с именем TextBlock с именем UserInput и кнопкой, подключенной к обработчику событий Click, который называется Button_Click. Путь к файлу необходимо изменить на файл, который существует на компьютере.";
+Employee employee = new() { Name = "Tommy", Birthday =  DateTime.Now, Salary = 150500m };
 
+Company yandex = new() { Title = "Yandex", City = "Moscow" };
+Company piterSoft = new() { Title = "Piter Soft", City = "St/ Peterburg" };
 
-//using (FileStream stream = File.Open(filePath, FileMode.OpenOrCreate))
+List<Employee> employees = new() 
+{ 
+    new() { 
+        Name = "Sammy", 
+        Birthday =  DateTime.Now.AddYears(-23), 
+        Salary = 90400m,
+        Labels = [100, 200, 300],
+        Company = yandex,
+    },
+    new() { 
+        Name = "Jonny", 
+        Birthday =  DateTime.Now.AddYears(-35), 
+        Salary = 75000m,
+        Labels = [5, 7],
+        Company = piterSoft,
+    },
+    new() { 
+        Name = "Billy", 
+        Birthday =  DateTime.Now.AddYears(-41), 
+        Salary = 120000m,
+        Labels = [1, 2, 3, 4, 5],
+        Company = yandex,
+    },
+};
+
+JsonSerializerOptions options = new()
+{
+    WriteIndented = true,
+};
+
+string json = JsonSerializer.Serialize<List<Employee>>(employees, options);
+Console.WriteLine(json);
+
+//using (FileStream writer = File.OpenWrite(fileJson))
 //{
-//    byte[] buffer = Encoding.UTF8.GetBytes(text);
-
-//    await stream.WriteAsync(buffer);
+//    //JsonSerializer.Serialize<Employee>(writer, employee);
+//    await JsonSerializer.SerializeAsync<List<Employee>>(writer, employees, options);
 //}
 
-//using (FileStream stream = File.Open(filePath, FileMode.Open))
+//using (FileStream reader = File.OpenRead(fileJson))
 //{
-//    byte[] buffer = new byte[stream.Length];
+//    List<Employee>? listEmpl = JsonSerializer.Deserialize<List<Employee>>(reader);
 
-//    await stream.ReadAsync(buffer, 0, buffer.Length);
-//    string textRead = Encoding.UTF8.GetString(buffer);
-
-//    Console.WriteLine(textRead);
+//    foreach(var e in listEmpl)
+//        Console.WriteLine($"{e.Name} {e.Birthday.ToLongDateString()} {e.Salary}");
 //}
 
 
+[DataContract(Name = "Customer")]
+class Employee
+{
+    [JsonPropertyName("FullName")]
+    public string? Name { get; set; }
 
+    public DateTime Birthday { get; set; }
 
+    [JsonIgnore]
+    public decimal Salary { get; set; }
 
+    public List<int> Labels { get; set; } = new();
 
+    public Company? Company { get; set; }
 
+}
 
+class Company
+{
+    public string? Title { get; set; }
+    public string? City { get; set; }
+}
